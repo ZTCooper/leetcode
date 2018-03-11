81b3f75b30e0c36f2e14076c73618a6686debb42
ztcooper <ztcooper@outlook.com>
2018-03-11 14:15:30 +0800

    Two Sum

--- 1.py
+++ 1.py
@@ -1,13 +1,17 @@
+'''
+使用buff dict降低算法复杂度
+'''
+
+
 class Solution(object):
     def twoSum(self, nums, target):
         if len(nums) <= 1:
             return
-        buff_dict = {}      #{3:0, 4:1,  }
+        buff_dict = {}  # {3:0, 4:1,  }
         for i in range(len(nums)):
             if nums[i] in buff_dict:
                 return [buff_dict[nums[i]], i]
             else:
                 buff_dict[target - nums[i]] = i
-                
+
                 #[3, 2, 4] 6
-                
